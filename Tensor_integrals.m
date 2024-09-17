de743abb6a9a7424d711a1b8ba0c1ff476d94f46
Ery McPartland <140166940+ery-mcpartland@users.noreply.github.com>
2024-09-17 13:23:04 +0100

    Automate creation of amplitude in Pi basis

--- Tensor_integrals.m
+++ Tensor_integrals.m
@@ -53,23 +53,27 @@ Needs["X`"]
 (*Amplitudes*)
 
 
-CreateAmplitude[num_,denom_]:=
+CreateAmplitude[num_,denom_]:=FullSimplify[(\[LeftAngleBracket]\[ScriptU][p-q,0], num ,\[ScriptU][p,mb]\[RightAngleBracket]//FermionLineExpand//Simplify )/denom]
 
 
 (* ::Subsection:: *)
 (*Replacements*)
 
 
-PropagatorReplace["a",amp_]:=
+PropagatorReplace["a",amp_]:=FullSimplify[amp /. {l . l->mc^2+P2,r . r->P4,l . r->1/2 (-P2+P3-P4),l . p->P7+P9,p . r->1/2 (-mb^2-P4+P8),l . q->P9,
+q . r->1/2 (-mb^2-P5+P8),p . q->1/2 (mb^2+q . q),p . p->mb^2}]
 
 
-PropagatorReplace["b",amp_]:=
+PropagatorReplace["b",amp_]:=FullSimplify[amp /. {l . l->mc^2+P2,r . r->P4,l . r->1/2 (-P2+P3-P4),l . p->P7+P9,p . r->1/2 (-P4+P8),l . q->P9,
+q . r->1/2 (-mb^2-P5+P8),p . q->1/2 (mb^2+q . q),p . p->mb^2}]
 
 
-PropagatorReplace["c",amp_]:=
+PropagatorReplace["c",amp_]:=FullSimplify[amp /. {l . l->mc^2+P2,r . r->P4,l . r->1/2 (-P2+P3-P4),l . p->1/2 (P1-P2+2 P7-q . q),
+p . r->1/2 (-P4+P5+2 P6),l . q->1/2 (P1-P2-q . q),q . r->P6,p . q->1/2 (mb^2+q . q),p . p->mb^2}]
 
 
-PropagatorReplace["d",amp_]:=
+PropagatorReplace["d",amp_]:=FullSimplify[amp /. {l . l->mc^2+P2,r . r->P4,l . r->1/2 (-P1+P12-P4-2 P6),l . p->1/2 (P1-P2+2 P7-q . q),p . r->(P11-P4)/2,
+l . q->1/2 (P1-P2-q . q),q . r->P6,p . q->1/2 (mb^2+q . q),p . p->mb^2}]
 
 
 PropagatorReplace["e",amp_]:=FullSimplify[amp /.{l . l->mc^2+P2,r . r->P4,l . r->1/2 (-P2+P3-P4),l . p->1/2 (P1-P2+2 P7-q . q),
@@ -88,23 +92,39 @@ p . r->1/2 (-P1+P12+2 P13+P2-P3),l . q->1/2 (P1-P2-q . q),q . r->1/2 (-P1+P12+P2
 (*Diagram a*)
 
 
+Solve[P2==l . l-mc^2&&P3==(l+r) . (l+r)-mc^2&&P4==r . r&&P5==(r+p-q) . (r+p-q)&&P8==(r+p) . (r+p)&&P7==l . (p-q)&&P9=={l . q}&&(p-q) . (p-q)==0&&(p . p)==mb^2,
+{l . l,r . r,l . r,l . p,r . p,l . q,r . q,p . q,p . p}]
+
+
 (* ::Subsubsection:: *)
 (*Diagram b*)
+
+
+Solve[P2==l . l-mc^2&&P3==(l+r) . (l+r)-mc^2&&P4==r . r&&P5==(r+p-q) . (r+p-q)-mb^2&&P8==(r+p) . (r+p)-mb^2&&P7==l . (p-q)&&P9=={l . q}&&(p-q) . (p-q)==0&&(p . p)==mb^2,
+{l . l,r . r,l . r,l . p,r . p,l . q,r . q,p . q,p . p}]
 
 
 (* ::Subsubsection:: *)
 (*Diagram c*)
 
 
+Solve[P1==(l+q) . (l+q)-mc^2&&P2==l . l-mc^2&&P3==(l+r) . (l+r)-mc^2&&P4==r . r&&P5==(r+p-q) . (r+p-q)&&P6==r . q&&P7==l . (p-q)&&(p-q) . (p-q)==0&&(p . p)==mb^2,
+{l . l,r . r,l . r,l . p,r . p,l . q,r . q,p . q,p . p}]
+
+
 (* ::Subsubsection:: *)
 (*Diagram d*)
+
+
+Solve[P1==(l+q) . (l+q)-mc^2&&P2==l . l-mc^2&&P12==(l+r+q) . (l+r+q)-mc^2&&P4==r . r&&P11==(r+p) . (r+p)-mb^2&&P6==r . q&&P7==l . (p-q)&&(p-q) . (p-q)==0&&(p . p)==mb^2,
+{l . l,r . r,l . r,l . p,r . p,l . q,r . q,p . q,p . p}]
 
 
 (* ::Subsubsection:: *)
 (*Diagram e*)
 
 
-Solve[P1==(l+q) . (l+q)-mc^2&&P2==l . l-mc^2&&P3==(l+r) . (l+r)-mc^2&&P4==r . r&&P7==l . (p-q)&&P13=={r . (p-q)}&&P12==(l+q+r) . (l+q+r)-mc^2,
+Solve[P1==(l+q) . (l+q)-mc^2&&P2==l . l-mc^2&&P3==(l+r) . (l+r)-mc^2&&P4==r . r&&P7==l . (p-q)&&P13==r . (p-q)&&P12==(l+q+r) . (l+q+r)-mc^2,
 {l . l,r . r,l . r,l . p,r . p,l . q,r . q}]
 
 
@@ -112,7 +132,13 @@ Solve[P1==(l+q) . (l+q)-mc^2&&P2==l . l-mc^2&&P3==(l+r) . (l+r)-mc^2&&P4==r . r&
 (*Testing*)
 
 
+numeratorE0I = FullSimplify[DiracMatrix[Subscript[\[Gamma], \[Alpha]],\[DoubleStruckCapitalP]L,\[Gamma] . (l+r)+mc \[DoubleStruckOne],Subscript[\[Gamma], \[Nu]],(\[Gamma] . l+mc \[DoubleStruckOne]),Subscript[\[Gamma], \[Nu]],\[Gamma] . (l+r)+mc \[DoubleStruckOne],Subscript[\[Gamma], \[Alpha]],\[DoubleStruckCapitalP]L,\[Gamma] . (p-q)+mb \[DoubleStruckOne],Subscript[\[Gamma], \[Mu]]]]
+
+
+CreateAmplitude[numeratorE0I,P2 (P3)^2 P4 mb^2]
+
+
 a = l . l + r . q
 
 
-PropagatorReplace["e",a]
+PropagatorReplace["e",CreateAmplitude[numeratorE0I,P2 (P3)^2 P4 mb^2]]
