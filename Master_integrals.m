5b7b27447d5ade22da1657a3fdf81fb46e72517b
Ery McPartland <140166940+ery-mcpartland@users.noreply.github.com>
2024-09-18 09:40:04 +0100

    Add draft code from results-for-2-loop-file

--- Master_integrals.m
+++ Master_integrals.m
@@ -113,3 +113,76 @@ MasterIntReduce["e",jAmp_]:=
 
 (* ::Subsection:: *)
 (*Testing*)
+
+
+diagramE0ii={1/(mb^2 P2 P3^2 P4 (mb-q) (mb+q)) (-2+\[ScriptD]) \[LeftAngleBracket]\[ScriptU][p-q,0],Subscript[\[Gamma], \[Mu]],\[DoubleStruckCapitalP]L,\[ScriptU][p,mb]\[RightAngleBracket] (mb^2 (4 mc^2 (-P12+P3)+(-2+\[ScriptD]) (-P3 (P1+P3-P4)+P12 (P2+P3-P4)))+(4 mc^2 (P12+2 P13-P3+2 P7)-(-2+\[ScriptD]) (-P3 (P1+P3-P4)+P12 (P2+P3-P4)+2 P13 (P2+P3-P4)+2 (P2-P4) P7)) q^2+(4 mc^2-(-2+\[ScriptD]) (P2-P4)) (mb-q) (mb+q) q . q)+1/(mb P2 P3^2 P4 (mb-q)^2 (mb+q)^2) 2 ((2-\[ScriptD]) (4 mc^2-(-2+\[ScriptD]) (P2+P3-P4)) (mb^2 (P1-P12+2 P13-P2+P3)+(-P1+P12+2 P13+P2-P3) q^2)+(-2+\[ScriptD]) (4 mc^2-(-2+\[ScriptD]) (P2-P4)) (mb^2 (P1-P2-2 P7)-(mb^2+P1-P2+2 P7) q^2+q^4)) \[LeftAngleBracket]\[ScriptU][p-q,0],\[DoubleStruckCapitalP]R,\[ScriptU][p,mb]\[RightAngleBracket] Subscript[p, \[Mu]]+1/(mb P2 P3^2 P4 (mb-q)^2 (mb+q)^2) 2 (-2+\[ScriptD]) (q^2 (4 mc^2 (P12+2 P13-P3+2 P7-q^2)-(-2+\[ScriptD]) (-P1 P3-P3^2+P12 (P2+P3-P4)+2 P13 (P2+P3-P4)+P3 P4+2 P2 P7-2 P4 P7+(-P2+P4) q^2))+mb^2 (4 mc^2 (-P12+2 P13+P3+2 P7+q^2)+(-2+\[ScriptD]) (-P1 P3-P3^2+P12 (P2+P3-P4)-2 P13 (P2+P3-P4)+P3 P4-2 P2 P7+2 P4 P7+(-P2+P4) q^2))) \[LeftAngleBracket]\[ScriptU][p-q,0],\[DoubleStruckCapitalP]R,\[ScriptU][p,mb]\[RightAngleBracket] Subscript[q, \[Mu]]}
+
+
+(* ::Section:: *)
+(*Automating process*)
+
+
+(* ::Subsection:: *)
+(*Initialisation and defining functions*)
+
+
+(* ::Input:: *)
+(*ClearAll[f]*)
+(*f=Replace[ExpandAll@#,Plus->List,1,Heads->True]&;*)
+
+
+(* ::Input:: *)
+(*gettingPowersFromSingleTerm["e",singleTerm_]:= Block[{powers,coeff},*)
+(*powers=Exponent[singleTerm,{1/P1,1/P2,1/P3,1/P4,1/P12,1/P7,1/P13}];*)
+(*coeff=Coefficient[singleTerm,(1/P1^#1 1/P2^#2 1/P3^#3 1/P4^#4 1/P12^#5 1/P7^#6 1/P13^#7) & @@ powers];*)
+(*{coeff,powers}*)
+(*]*)
+
+
+(* ::Subsection:: *)
+(*Extracting correct integral*)
+
+
+(* ::Input:: *)
+(*cTypeIntegral=Part[FullSimplify[(Coefficient[diagramE0ii,\[LeftAngleBracket]\[ScriptU][p-q,0],Subscript[\[Gamma], \[Mu]],\[DoubleStruckCapitalP]L,\[ScriptU][p,mb]\[RightAngleBracket] ])mb^2(mb-q)(mb+q)],1]*)
+
+
+(* ::Input:: *)
+(*1/(P2 P3^2 P4) (-2+\[ScriptD]) (mb^2 (4 mc^2 (-P12+P3)+(-P3 (P1+P3-P4)+P12 (P2+P3-P4)) (-2+\[ScriptD]))+q^2 (4 mc^2 (P12+2 P13-P3+2 P7)-(-P3 (P1+P3-P4)+P12 (P2+P3-P4)+2 P13 (P2+P3-P4)+2 (P2-P4) P7) (-2+\[ScriptD]))+(mb-q) (mb+q) (4 mc^2-(P2-P4) (-2+\[ScriptD])) q . q)*)
+
+
+(* ::Input:: *)
+(*oldIntegralList=Part[f/@{cTypeIntegral},1]*)
+(**)
+
+
+(* ::Input:: *)
+(*For[newIntegralList={};i=1,i<(Length[oldIntegralList]+1),i++,AppendTo[newIntegralList,gettingPowersFromSingleTerm["e",Part[1/(mb^2(mb-q)(mb+q)) oldIntegralList,i]]]]*)
+
+
+finalIntegral=FullSimplify[Total[newIntegralList/.{b_,{a1_,a2_,a3_,a4_,a5_,a6_,a7_}}->b j[e0,a1,a2,a3,a4,a5,a6,a7]]/.q . q->q^2]
+
+
+{x+2x^2,1+x,70x^2+3x^3}
+
+
+Coefficient[{x+2x^2,1+x,70x^2},x^2]
+
+
+Exponent[{x+2x^2,1+x,70x^2+3x^3},x]
+
+
+gettingPowersFromSingleTerm["e",singleTerm_]:= Block[{powers,coeff},
+powers=Exponent[singleTerm,{1/P1,1/P2,1/P3,1/P4,1/P12,1/P7,1/P13}];
+coeff=Coefficient[singleTerm,(1/P1^#1 1/P2^#2 1/P3^#3 1/P4^#4 1/P12^#5 1/P7^#6 1/P13^#7) & @@ powers];
+{coeff,powers}
+]
+
+
+{-((4 mb^2 P12)/(P2 P3^2)),(4 \[ScriptD] mb^2 P12)/(P2 P3^2),-((\[ScriptD]^2 mb^2 P12)/(P2 P3^2))}
+
+
+gettingPowersFromSingleTerm["e",{-((4 mb^2 P12)/(P2 P3^2)),(4 \[ScriptD] mb^2 P12)/(P2 P3^2),-((\[ScriptD]^2 mb^2 P12)/(P2 P3^2))}]
+
+
+
